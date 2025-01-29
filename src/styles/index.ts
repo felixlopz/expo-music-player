6b9ced6d5e0e21af211a02beff751ebfb6a6e4a8
felixlopz <felixlopzd@gmail.com>
2025-01-28 21:53:43 -0400

    routing

---
+++ src/styles/index.ts
@@ -0,0 +1,15 @@
+import { colors, fonSize } from '#/contants/tokens'
+import { StyleSheet } from 'react-native'
+
+export const defaultStyles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: colors.background,
+	},
+	text: {
+		fontSize: fonSize.base,
+		color: colors.text,
+	},
+})
+
+export const utilsStyles = StyleSheet.create({})
